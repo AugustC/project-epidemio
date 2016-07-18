15dc9ff13cf92ece20f4aaa3d362ef1d39e5d738
AugustC <augusto.cms7@gmail.com>
2016-07-18 15:01:15 -0300

    City class finished

--- src/City.py
+++ src/City.py
@@ -1,15 +1,14 @@
 class City:
+    """Class of cities"""
     
-    def _init_(self, cityname, disease_frequency, population):
+    def __init__(self, cityname, disease_frequency, population):
         self.name = cityname
         self.frequency = disease_frequency
         self.population = population
-        self.outbreaks = city.find_outbreaks()
+        self.outbreaks = self.find_outbreaks()
 
-    def find_outbreaks():
+    def find_outbreaks(self):
         # Function that creates a boolean array with value true where an epidemic outbreak occurs
         # Outbreak: Infected people surpass 300 per thousand of habitants
-        pass
-
-    
-    
+        outbreak = [(self.frequency[i]/self.population) >= 300 for i in range(len(self.frequency)) ]
+        return outbreak
