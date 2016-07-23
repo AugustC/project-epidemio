ac238ce68ffa61eefdd2673b258b128a7cb0db4b
AugustC <augusto.cms7@gmail.com>
2016-07-23 18:10:08 -0300

    data_to_frequency definition corrected

--- src/dataset.py
+++ src/dataset.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
 def read_input(filename):
-    # Receive input from filename and return the dataset of the file
+    # Receive input from filename.csv and return the dataset of the file
     pass
 
 def data_to_frequency(dataset):
-    # Receive the dataset and returns a vector of City (see class City)
+    # Data pre-processing that receives the dataset and returns a vector of objects of the class City
     pass
