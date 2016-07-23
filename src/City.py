fa0d98bb748340336553df0c38551a5ea422b06b
AugustC <augusto.cms7@gmail.com>
2016-07-23 18:05:56 -0300

    Outbreak definition corrected

--- src/City.py
+++ src/City.py
@@ -10,5 +10,5 @@ class City:
     def find_outbreaks(self):
         # Function that creates a boolean array with value true where an epidemic outbreak occurs
         # Outbreak: Infected people surpass 300 per thousand of habitants
-        outbreak = [(self.frequency[i]/self.population) >= 300 for i in range(len(self.frequency)) ]
+        outbreak = [(self.frequency[i]/self.population) >= 0.3 for i in range(len(self.frequency)) ]
         return outbreak
