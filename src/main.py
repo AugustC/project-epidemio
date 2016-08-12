68d9cc39d7cf86604ed015eff390aa6d2e3a0876
AugustC <augusto.cms7@gmail.com>
2016-08-11 22:14:17 -0300

    Created main

--- src/main.py
+++ src/main.py
@@ -1,19 +1,28 @@
-import correlations
-import dataset
+import correlations as cor
+import dataset as dt
+import runtime_functions as rt
 
-# Implement GUI for input
+# Implement GUI
+user_choices = {
+    0 : "Fechar o programa",
+    1 : "Listar as cidades do arquivo",
+    2 : "Escolher uma cidade especifica"
+    }
+
+# Main
 filename = input("Entre com o nome do arquivo csv: ")
-dataset = read_input(filename)
+dataset = dt.read_input(filename)
 
-cities = data_to_frequency(dataset)
+user_input = 1
+while(user_input != 0):
+    print("Selecione o que deseja fazer:")
+    print(user_choices)
+    user_input = int(input())
+    
+    # Input verification (change to dict default of user_input?)
+    while user_input not in [0, 1, 2]:
+        print("Por favor, escolha entre 0, 1 e 2")
+        user_input = int(input())
 
-# User choices (correlation, graphs)
+    rt.user_interaction(user_input, dataset)
 
-""" 
-correlation between cities := correlation of the amount of infected people between city1 and city2
-Disease network
- - For each epidemic outbreak of each city, do the correlation between the city and all the other cities.
-   If the correlation is above a determined threshold, put the cities in a cluster.
-   Do it again, with a delay of one, two and three weeks between city1 and city2. 
-   If the correlation is above the threshold, put a link between city1 and city2.
-"""
