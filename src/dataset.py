9a84e0a0cd32309b319cd10ef94c776ca39dbf08
AugustC <augusto.cms7@gmail.com>
2016-08-17 18:46:17 -0300

    read_population added

--- src/dataset.py
+++ src/dataset.py
@@ -4,9 +4,22 @@ import numpy as np
 first_year = 2000
 last_year = 2013
 
-def read_input(filename):
-    # Receive input from filename.csv and return the panda time series
+def read_raw_input(filename):
+    # Receive the raw input from filename.csv and return the panda time series
     dataset = pd.read_csv(filename, sep=';')
     datagroup = dataset.groupby(['CIDADE', 'NU_ANO', 'SEM_NOT'])
     series = datagroup.size()
     return series
+
+def read_frequency_input(filename):
+    # Receive the frequency input from filename.csv and return the panda time series
+    pass
+
+def read_population(filename):
+    # Receive the file with the cities and number of inhabitants
+    d = {}
+    with open(filename) as f:
+        for line in f:
+            (key, val) = line.split()
+            d[key] = val
+    return d
