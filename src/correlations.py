7299089ebcbbcce6392ad3e562ba4b985aabef3c
AugustC <augusto.cms7@gmail.com>
2016-08-17 18:44:54 -0300

    find_outbreaks updated

--- src/correlations.py
+++ src/correlations.py
@@ -1,4 +1,5 @@
 import scipy.stats as sp
+import numpy as np
 
 def pearson_correlation(time_series1, time_series2, start, maxdelay=0, time=4):
     # Pearson correlation of the disease frequency in time_series1 and time_series2
@@ -20,11 +21,29 @@ def pearson_correlation(time_series1, time_series2, start, maxdelay=0, time=4):
             
     return max_correlation, correlation_delay
 
+def two_derivatives(city, week):
+    D1 = (city[week + 1] - city[week])/2
+    D2 = (city[week + 2] - city[week + 1])/2
+    if D1 > 0 and D2 > 0:
+        return True
+    return False
 
-def find_outbreaks(timeseries, population):
-    # Function that creates a boolean array with value True where an epidemic outbreak occurs
-    # Outbreak: Infected people surpass 300 per thousand of habitants
-    outbreak = timeseries/population > 0.3
+def find_outbreaks(city, population):
+    # Function that returns a boolean array with the value true for each year where a significant outbreak occurred
+    # Outbreak: Amount of infected people surpass 300 per 100000 of inhabitants
+    years = city.index.levels[0]
+    outbreak = []
+    for year in years:
+        city_year = city[year].values
+        sum = 0;
+        tmp = [year, False, 0]
+        for i in range(len(city_year)):
+            sum = city_year[i]
+            if sum >= (3 / 1000): # Amount of notified diseases surpassed 300 per 100000 of inhabitants 
+                if two_derivatives(city_year, i): # The disease frequency went up after that week
+                    tmp = [year, True, i]
+                    outbreak.append(tmp)
+                    break
     return outbreak
 
 
