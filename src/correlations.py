4be9e15a9d2bd9237f7b3bd9ecd0044f1b3b5a19
AugustC <augusto.cms7@gmail.com>
2016-08-25 20:25:14 -0300

    find_outbreaks and pearson correlations adjusted.

--- src/correlations.py
+++ src/correlations.py
@@ -1,50 +1,38 @@
 import scipy.stats as sp
 import numpy as np
 
-def pearson_correlation(time_series1, time_series2, start, maxdelay=0, time=4):
-    # Pearson correlation of the disease frequency in time_series1 and time_series2
-    # between weeks 'start' and 'start + time'.
-    # If maxdelay is greater than 0, the function returns the maximum correlation between time_series1
-    # and time_series2 with delays (0 <= delays <= maxdelay) applied on the second time_series, and the
-    # delay number that gave the maximum correlation
+def pearson_correlation(time_series1, time_series2, start, delay=0):
+    # Pearson correlation of the disease frequency in time_series1 and time_series2    
+    x = time_series1[:-delay]
+    y = time_series2[delay:]
+    corr = sp.pearsonr(x, y)[0]
+    return corr
 
-    correlation_delay = 0
-    max_correlation = -1
-    
-    for delay in range(maxdelay + 1):
-        x = time_series1[start : start + time]
-        y = time_series2[start + delay : start + time + delay]
-        corr = sp.pearsonr(x, y)[0]
-        if corr > max_correlation:
-            max_correlation = corr
-            correlation_delay = delay
-            
-    return max_correlation, correlation_delay
+def cities_correlations(city1, city2, dataset, year):
+    # Function that receives two arrays of (city, week) and perform the correlation between city 1 and city 2
+    # between the year passed as argument
 
 def two_derivatives(city, week):
+    # Function that returns true if the disease_frequency still grows after it reached 300 per 100000 of inhabitants
+    if (len(city) - 2 <= week):
+        return False
     D1 = (city[week + 1] - city[week])/2
     D2 = (city[week + 2] - city[week + 1])/2
     if D1 > 0 and D2 > 0:
         return True
     return False
 
-def find_outbreaks(city, population):
-    # Function that returns a boolean array with the value true for each year where a significant outbreak occurred
-    # Outbreak: Amount of infected people surpass 300 per 100000 of inhabitants
-    years = city.index.levels[0]
-    outbreak = []
-    for year in years:
-        city_year = city[year].values
-        sum = 0;
-        tmp = [year, False, 0]
-        for i in range(len(city_year)):
-            sum = city_year[i]
-            if sum >= (3 / 1000): # Amount of notified diseases surpassed 300 per 100000 of inhabitants 
-                if two_derivatives(city_year, i): # The disease frequency went up after that week
-                    tmp = [year, True, i]
-                    outbreak.append(tmp)
-                    break
-    return outbreak
+def find_outbreaks(dataset, city, year):
+    # Function that returns true if an outbreak occurred in that city, during that year,
+    # and the week that the outbreak started. If there wasn't an outbreak that year, week returns -1
+    disease_frequency = dataset[city][year].values
+    count = 0;
+    for i in range(len(disease_frequency)):
+        count = count + disease_frequency[i]
+        if count >= (3 / 1000):
+            if two_derivatives(disease_frequency, i):
+                return True, i
+    return False, -1
 
 
 def derivative_correlation(time_series1, time_series2, delay=0, start=0, time=4):
