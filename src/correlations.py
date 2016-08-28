96f525fee6dbe9179d4a51fac9c6d87ef5da0b65
AugustC <augusto.cms7@gmail.com>
2016-08-28 19:17:09 -0300

    cities_correlation added

--- src/correlations.py
+++ src/correlations.py
@@ -1,17 +1,24 @@
 import scipy.stats as sp
 import numpy as np
 
-def pearson_correlation(time_series1, time_series2, start, delay=0):
+def pearson_correlation(time_series1, time_series2, delay=0):
     # Pearson correlation of the disease frequency in time_series1 and time_series2    
     x = time_series1[:-delay]
     y = time_series2[delay:]
     corr = sp.pearsonr(x, y)[0]
     return corr
 
-def cities_correlations(city1, city2, dataset, year):
+def cities_correlations(city1, city2, dataset, year, delay, threshold):
     # Function that receives two arrays of (city, week) and perform the correlation between city 1 and city 2
-    # between the year passed as argument
-
+    # in the year passed as argument, with a delay between the weeks,
+    for c1 in city1:
+        for c2 in city2:
+            cor_city1 = dataset[c1[0]][year].values
+            cor_city2 = dataset[c2[0]][year].values
+            pcor = pearson_correlation(cor_city1, cor_city2, delay)
+            if pcor > threshold:
+                print(c1[0] + " --> " + c2[0] + " with delay " + str(delay))
+    
 def two_derivatives(city, week):
     # Function that returns true if the disease_frequency still grows after it reached 300 per 100000 of inhabitants
     if (len(city) - 2 <= week):
