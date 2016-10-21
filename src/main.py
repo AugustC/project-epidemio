73b62ca096babbffeb98efde79dfa5bd714ec1d9
AugustC <augusto.cms7@gmail.com>
2016-10-21 13:43:01 -0200

    First level graph

--- src/main.py
+++ src/main.py
@@ -1,6 +1,5 @@
 import correlations as cor
 import dataset as dt
-import runtime_functions as rt
 import numpy as np
 
 # Read user input
@@ -15,7 +14,7 @@ else:
     dataset = dt.read_frequency_input(data_file)
 
 focus_time = int(input("Entre com a quantidade de semanas do foco: "))
-k = 0.8
+k = float(input("Entre com o threshold da correlacao: "))
 
 # First filter
 cities = dataset.index.levels[0]
@@ -49,9 +48,11 @@ for year in years:
     
     # S1, first graph level
     S1 = [city for city in outbreak_cities if city not in focus]
-    S1_cities = cor.correlation_cities(focus, S1, dataset, year, 0, k)
+    S1_cities = cor.correlation_cities(focus, S1, dataset, year, 1, k)
 
     print()
     print("Cidades que tiveram correlacao maior que " + str(k) + " com delay 0")
     for cities in S1_cities:
         print(cities)
+
+    S2 = [city for city in outbreak_cities if city not in focus]
