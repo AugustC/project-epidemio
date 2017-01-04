3ba801f7da4c36ef8dc9fc722a48ec2c9046ce71
Augusto <augusto.cms7@gmail.com>
2017-01-04 03:42:08 -0200

    Dados de entrada arrumados

--- src/dataset.py
+++ src/dataset.py
@@ -1,3 +1,4 @@
+# -*- coding: latin-1 -*-
 import pandas as pd
 import numpy as np
 
@@ -6,9 +7,31 @@ last_year = 2013
 
 def read_raw_input(filename):
     # Receive the raw input from filename.csv and return the panda time series
-    dataset = pd.read_csv(filename, sep=';')
-    datagroup = dataset.groupby(['CIDADE', 'NU_ANO', 'SEM_NOT'])
-    series = datagroup.size()        
+    dataset = pd.read_csv(filename, sep=';', encoding='latin-1')
+
+    labels = dataset.keys()
+    city_label = labels[0]
+    cities = dataset[city_label]
+    weeks_str = labels[1:]
+    weeks_int = weeks_str.map(int)
+    years = np.unique(np.floor(weeks_int/100))
+
+    matrix = np.empty((0, 52))
+    tuples_cityear = []
+    for cityI in range(len(cities)):
+        for year in years:
+            tuples_cityear.append((cities[cityI],str(int(year)))) # index
+            notified = []
+            for week in weeks_int:
+                if (week >= year * 100 and week < (year + 1) * 100):
+                    notified.append(dataset.get_value(cityI, str(week)))
+            if len(notified) > 52:
+                notified = notified[:52] # limit the amount of weeks in a year to 52
+            matrix = np.vstack((matrix, notified))
+            
+    index = pd.MultiIndex.from_tuples(tuples_cityear, names=['Cidade', 'Ano'])
+    series = pd.DataFrame(matrix, index=index)
+    series = series.T.unstack()
     return series
 
 def read_frequency_input(filename):
@@ -21,14 +44,17 @@ def read_frequency_input(filename):
 def read_population(filename):
     # Receive the file with the cities and number of inhabitants
     d = {}
-    with open(filename) as f:
+    with open(filename, encoding="latin-1") as f:
         for line in f:
-            (key, val) = line.split()
-            d[key] = val
+            (key, val) = line.split(";")
+            d[key] = int(val)
     return d
 
 def weight_population(dataset, population):
     # Function that receives a dataset of the disease and the population of each city
     # and returns a disease frequency series, weighted by the population of each city
-    pass
+    cities = dataset.index.levels[0]
+    for city in cities:
+        dataset.T[city] = dataset.T[city]/population[city]
+    return dataset
 
--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import correlations as cor
 import dataset as dt
 import numpy as np
@@ -9,7 +10,7 @@ if aux == '1':
     dataset = dt.read_raw_input(data_file)
     population_file = input("Entre com o nome do arquivo das populacoes: ")
     population = dt.read_population(population_file)
-    dataset = dt.weight_population(dataset1, population)
+    dataset = dt.weight_population(dataset, population)
 else:
     dataset = dt.read_frequency_input(data_file)
 
@@ -29,30 +30,32 @@ for year in years:
     outbreaks[year] = outbreak_cities
 
 # Find graph
-for year in years:
-    focus = []
-    outbreak_cities = outbreaks[year]
-    outbreak_cities.sort(key=lambda x : x[1])
-    i = 0
-    
+# for year in years:              
+year = "2008"                     # user input
+focus = []
+outbreak_cities = outbreaks[year]
+outbreak_cities.sort(key=lambda x : x[1])
+i = 0
+
     # Focus
-    startweek_focus = outbreak_cities[0][1]
-    endweek_focus = startweek_focus + focus_time
-    while outbreak_cities[i][1] <= endweek_focus:
-        focus.append(outbreak_cities[i])
-        i = i + 1
-    print()
-    print(year)
-    f_print = [city[0] for city in focus]
-    print("Focos da doenca: ", f_print)
+startweek_focus = outbreak_cities[0][1]
+endweek_focus = startweek_focus + focus_time
+while outbreak_cities[i][1] <= endweek_focus:
+    focus.append(outbreak_cities[i])
+    i = i + 1
+print()
+print(year)
+f_print = tuple(city[0] for city in focus)
+print("Focos da doenca: ", f_print)
     
-    # S1, first graph level
-    S1 = [city for city in outbreak_cities if city not in focus]
-    S1_cities = cor.correlation_cities(focus, S1, dataset, year, 1, k)
+# S1, first graph level
+S1 = [city for city in outbreak_cities if city not in focus]
+S1_cities = cor.correlation_cities(focus, S1, dataset, year, 1, k)
 
-    print()
-    print("Cidades que tiveram correlacao maior que " + str(k) + " com delay 0")
-    for cities in S1_cities:
-        print(cities)
-
-    S2 = [city for city in outbreak_cities if city not in focus]
+print()
+print("Cidades que tiveram correlacao maior que " + str(k) + " com delay 0")
+for cities in S1_cities:
+    print(cities)
+    
+S2 = [city for city in outbreak_cities if city not in focus]
+    
