f979e1bb336cc62c3bfb38194b4d3b82ad744053
Augusto <augusto.cms7@gmail.com>
2017-01-16 13:00:52 -0200

    Loop added to find other levels of the graph

--- src/main.py
+++ src/main.py
@@ -22,52 +22,59 @@ k = float(input("Entre com o threshold da correlacao: "))
 # First filter (Use only cities where an outbreak occurred)
 cities = dataset.index.levels[0]
 years = dataset.index.levels[1]
-outbreaks = {}
+all_outbreaks = {}
 for year in years:
-    outbreak_cities = []
+    outbreak = []
     for city in cities:
         occurred_outbreak, week = cor.find_outbreaks(dataset, city, year)
         if occurred_outbreak:
-            outbreak_cities.append((city, week))
-    outbreaks[year] = outbreak_cities
+            outbreak.append((city, week))
+    all_outbreaks[year] = outbreak
 
 # Find graph
 year = input("Entre com o ano desejado: ")                     # user input
 focus = []
 g = nx.DiGraph()
-outbreak_cities = outbreaks[year]
-outbreak_cities.sort(key=lambda x : x[1])
+outbreak = all_outbreaks[year]
+outbreak.sort(key=lambda x : x[1])
 i = 0
 
 # Focus
-startweek_focus = outbreak_cities[0][1]
+startweek_focus = outbreak[0][1]
 endweek_focus = startweek_focus + focus_time
-while outbreak_cities[i][1] <= endweek_focus:
-    focus.append(outbreak_cities[i])
+while outbreak[i][1] <= endweek_focus:
+    focus.append(outbreak[i][0])
     i = i + 1
-    g.add_node(outbreak_cities[i][0])
-f_print = tuple(city[0] for city in focus)
-print("Focos da doenca: ", f_print)
-    
-# S1, first graph level
-S1 = [city for city in outbreak_cities if city not in focus]
-S1_cities = cor.correlation_cities(focus, S1, dataset, year, 1, k)
+    g.add_node(outbreak[i][0])
 
-print()
-print("Cidades que tiveram correlacao maior que " + str(k) + " com delay 0")
-for cities in S1_cities:
-    print(cities)
-    g.add_node(cities[1])    
-    g.add_edge(cities[0], cities[1])
+# Other levels of the graph
+remains = [city[0] for city in outbreak if city[0] not in focus]
+i = 1 
+all_city_correlations = []
+levels = [focus]
+while remains:
+    # Do the correlation between the cities on the graph and the remaining ones with a delay
+    city_correlation = cor.correlation_cities(focus, remains, dataset, year, i, k)
+    for j in range(1,i):
+        l = cor.correlation_cities(levels[j - 1], remains, dataset, year, i - j, k)
+        if l:
+            city_correlation.extend(l)
+    if not city_correlation:                   # there's no more correlations
+        g.add_nodes_from(remains)
+        remains = []
+    else:                                         # add another level
+        g.add_nodes_from([x[1] for x in city_correlation])
+        g.add_edges_from([(x[0], x[1], { 'weight' : x[2]}) for x in city_correlation])
+        all_city_correlations.append(city_correlation)
+        new_cities = [x[1] for x in city_correlation]
+        levels.append(new_cities)
+        remains = [city for city in remains if city not in new_cities]
+        i += 1
 
-# TODO: Change color of focus
+# TODO: Change color of focus, put weight(delay) on edges
 pos_spring = nx.spring_layout(g)
 nx.draw_networkx(g, with_labels=False, arrows=True, node_color='y', linewidth=0, pos=pos_spring)
 for p in pos_spring:
-    pos_spring[p][1] += 0.03
+    pos_spring[p][1] += 0.05
 nx.draw_networkx_labels(g, pos_spring)
 plt.show()
-
-# S2 = [city for city in outbreak_cities if city not in focus]
-    
-
